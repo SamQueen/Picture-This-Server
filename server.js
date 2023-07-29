bdc015212497ff43a6ef718c84c6d4f67b2f4181
SamQueen <samqueenbusiness@gmail.com>
2023-07-29 15:39:26 -0700

    cors configuration

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ require('dotenv').config()
 const PORT = 5000;
 
 // middleware
-/*app.use(cors({
-    origin: ["https://picture-this-project.herokuapp.com/" + process.env.PORT],
+app.use(cors({
+    origin: ["http://localhost:3000"],
     methods: ["POST", "GET"],
     credentials: true
-}));*/
-app.use(cors());
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
